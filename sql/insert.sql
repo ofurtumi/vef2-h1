13ebabd288eb6c8416471f02ef88b160ea9e3e6e
Tumi <thorvaldur.tumi@gmail.com>
2022-03-14 14:39:20 +0000

    bætt aftur cart

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,42 +1,235 @@
-INSERT INTO categories
-(cat_id,title)
-VALUES
-('27dc4dc7-7a06-4306-9923-f124066747fa','burgers'),
-('e7cfb87e-d881-4d23-91b1-47295846ef42','pizzas'),
-('02726ec2-192f-4971-a01c-60964179588f','salads'),
-('1a209e14-5b00-4986-bb72-ac0390e8b176','soups');
-
-INSERT INTO menuitems
-(title, price, description, image, categoryid)
-VALUES 
-('cheeseburger',500,'burger with cheese on it', '0dd96d5718f3fa2a04e853110e757817_tlqdlb', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('hamburger',350,'burger with no cheese', 'linkToImage', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('veganburger',500,'burger with no meat', 'linkToImage', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('meat paradise burger',750,'burger with lots of meat', 'linkToImage', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('margherita', 790, 'pizza with cheese', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('pepperoni pizza', 990, 'pizza with pepperoni', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('hawaiian', 990, 'pizza with pineapple and ham', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('meat paradise pizza', 1290, 'pizza with lots of different meat', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('ceasar salad', 500, 'classic ceasar salad', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('chicken salad', 790, 'fancy chicken with salad on top', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('greek salad', 790, 'salad from east? europe', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('meat paradise salad', 1090, 'all of our meat in a bowl and a tomato', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('good soup', 490, 'soup of the day, mmm good soup', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('french onion soup', 750, 'oui oui baguette', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('bean soup', 750, 'salt meat and beans, two coins', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('Hamborger of the month', 950, 'Ask the waiter what the burger is this month', 'linkToImage', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('Fat bastard', 1020, '150g Burger, spicy mayo, beef brisket, aged Tindur cheese, tomatoes and onion served with tomato relish, pickled jalapeno, pickles and french fries', 'linkToImage', '27dc4dc7-7a06-4306-9923-f124066747fa'),
-('skinny one', 870, 'Burger with Portobello mushroom, baked garlic aioli, arugula, tomato relish and french fries','linkToImage','27dc4dc7-7a06-4306-9923-f124066747fa'),
-('Parma Rucola', 790, 'Parma ham, rucola, black pepper and pesto', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('Mexican', 790, 'Chicken, red onion, jalapeno, nachos and cheese blend', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('Hot date', 890, 'Cheese blend, chorizon, bacon, dates and cream cheese', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('Baby pizza', 370, 'small pizza with one topping', 'linkToImage', 'e7cfb87e-d881-4d23-91b1-47295846ef42'),
-('Tex mex', 600, 'Spinach, iceberg and romain salad with chicken, corn and deepfried jalapeno', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('Oriental', 500, 'Spinach, ruccola and romain salad with chicken, edamame, beans and mango', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('Falafel', 460, 'Spinach, ruccola and romain salad with falafel balls', 'linkToImage', '02726ec2-192f-4971-a01c-60964179588f'),
-('Cobb salad', 600, 'Spinch, ruccola and romain salad with chicken thighs, eggs and avocado', 'linkToImage','02726ec2-192f-4971-a01c-60964179588f'),
-('Mushroom soup', 670, 'Creamy mushroom soup with wild mushrooms', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('Chicken soup', 700 , 'The way mama wanted aspired to make', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('Aspargus soup', 560, 'Cream of aspargus', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('meat paradise soup', 750, 'meat and water', 'linkToImage', '1a209e14-5b00-4986-bb72-ac0390e8b176'),
-('testpizza',1000,'test description','testLinkToImage','e7cfb87e-d881-4d23-91b1-47295846ef42');
+INSERT INTO categories (cat_id, title)
+VALUES (
+        '27dc4dc7-7a06-4306-9923-f124066747fa',
+        'burgers'
+    ),
+    ('e7cfb87e-d881-4d23-91b1-47295846ef42', 'pizzas'),
+    ('02726ec2-192f-4971-a01c-60964179588f', 'salads'),
+    ('1a209e14-5b00-4986-bb72-ac0390e8b176', 'soups');
+INSERT INTO menuitems (title, price, description, image, categoryid)
+VALUES (
+        'cheeseburger',
+        500,
+        'burger with cheese on it',
+        '0dd96d5718f3fa2a04e853110e757817_tlqdlb',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'hamburger',
+        350,
+        'burger with no cheese',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'veganburger',
+        500,
+        'burger with no meat',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'meat paradise burger',
+        750,
+        'burger with lots of meat',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'margherita',
+        790,
+        'pizza with cheese',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'pepperoni pizza',
+        990,
+        'pizza with pepperoni',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'hawaiian',
+        990,
+        'pizza with pineapple and ham',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'meat paradise pizza',
+        1290,
+        'pizza with lots of different meat',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'ceasar salad',
+        500,
+        'classic ceasar salad',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'chicken salad',
+        790,
+        'fancy chicken with salad on top',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'greek salad',
+        790,
+        'salad from east? europe',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'meat paradise salad',
+        1090,
+        'all of our meat in a bowl and a tomato',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'good soup',
+        490,
+        'soup of the day, mmm good soup',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'french onion soup',
+        750,
+        'oui oui baguette',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'bean soup',
+        750,
+        'salt meat and beans, two coins',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'Hamborger of the month',
+        950,
+        'Ask the waiter what the burger is this month',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'Fat bastard',
+        1020,
+        '150g Burger, spicy mayo, beef brisket, aged Tindur cheese, tomatoes and onion served with tomato relish, pickled jalapeno, pickles and french fries',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'skinny one',
+        870,
+        'Burger with Portobello mushroom, baked garlic aioli, arugula, tomato relish and french fries',
+        'linkToImage',
+        '27dc4dc7-7a06-4306-9923-f124066747fa'
+    ),
+    (
+        'Parma Rucola',
+        790,
+        'Parma ham, rucola, black pepper and pesto',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'Mexican',
+        790,
+        'Chicken, red onion, jalapeno, nachos and cheese blend',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'Hot date',
+        890,
+        'Cheese blend, chorizon, bacon, dates and cream cheese',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'Baby pizza',
+        370,
+        'small pizza with one topping',
+        'linkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    ),
+    (
+        'Tex mex',
+        600,
+        'Spinach, iceberg and romain salad with chicken, corn and deepfried jalapeno',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'Oriental',
+        500,
+        'Spinach, ruccola and romain salad with chicken, edamame, beans and mango',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'Falafel',
+        460,
+        'Spinach, ruccola and romain salad with falafel balls',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'Cobb salad',
+        600,
+        'Spinch, ruccola and romain salad with chicken thighs, eggs and avocado',
+        'linkToImage',
+        '02726ec2-192f-4971-a01c-60964179588f'
+    ),
+    (
+        'Mushroom soup',
+        670,
+        'Creamy mushroom soup with wild mushrooms',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'Chicken soup',
+        700,
+        'The way mama wanted aspired to make',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'Aspargus soup',
+        560,
+        'Cream of aspargus',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'meat paradise soup',
+        750,
+        'meat and water',
+        'linkToImage',
+        '1a209e14-5b00-4986-bb72-ac0390e8b176'
+    ),
+    (
+        'testpizza',
+        1000,
+        'test description',
+        'testLinkToImage',
+        'e7cfb87e-d881-4d23-91b1-47295846ef42'
+    );
+INSERT INTO cart (cart_id)
+VALUES ('ae66c235-6b24-47fc-8bc8-c4b8aa838f74');
+INSERT INTO cartline (itemid, cartid, num)
+VALUES (1, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 2),
+    (5, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 1),
+    (19, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 4),
+    (7, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 3),
+    (8, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 4),
+    (15, 'ae66c235-6b24-47fc-8bc8-c4b8aa838f74', 2);
